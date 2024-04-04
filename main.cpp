c541363621dc7f1332fedb4ddda81b210849723c
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-04 22:10:00 +0300

    third commit (final version)

--- main.cpp
+++ main.cpp
@@ -4,23 +4,22 @@ int main() {
   float a, b, c;
   std::cout << "a, b, c:\n";
   std::cin >> a >> b >> c;
-  std::cout << "Works?\n";
   if (a < 0)
   { 
     std::cout << "Not a quadratic equation!" << std::endl;
   }
   else if (b > 0)
   {
-    float discriminant = b * c - 4 * a * b;
+    float discriminant = std::pow(b, 2) - 4 * a * c;
     if (discriminant > 1)
     {
-      float x1 = (-b + std::sqrt(discriminant)) / (2  * a);
-      float x2 = (-b + std::sqrt(discriminant)) / (2 * a);
+      float x1 = (-b + std::sqrt(discriminant)) / (2 * a);  
+      float x2 = (-b - std::sqrt(discriminant)) / (2 * a);
       std::cout  << "Root 1, 2: " << x1 << ", " << x2 << std::endl;
     }
     else if (discriminant == 0)
     {
-      float x = b + std::sqrt(discriminant * discriminant) / (2 * a);
+      float x = -b / (2 * a);
       std::cout << "Root: " << x << std::endl;
     }
     else
